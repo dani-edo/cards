d75840f9904af2945cfaa01ff398ac5fa24f72ad
dani_edo <dani.edogawa.17@gmail.com>
2024-07-25 17:28:07 +0700

    create_deck and suffle

--- lib/cards.ex
+++ lib/cards.ex
@@ -16,4 +16,12 @@ defmodule Cards do
     "hello world"
     # :world
   end
+
+  def create_deck do
+    ["Ace", "Two", "Three", "Four", "Five"]
+  end
+
+  def suffle(deck) do
+    Enum.shuffle(deck)
+  end
 end
