30e2d84c6efb8f84adf40549173e7de3c0f4ed58
dani_edo <dani.edogawa.17@gmail.com>
2024-07-27 00:30:00 +0700

    use multiple function without using pipe

--- lib/cards.ex
+++ lib/cards.ex
@@ -55,4 +55,10 @@ defmodule Cards do
       {:error, _reason} -> "File does not exist"
     end
   end
+
+  def create_hand(hand_size) do
+    deck = Cards.create_deck
+    deck = Cards.shuffle(deck)
+    hand = Cards.deal(deck, hand_size)
+  end
 end
