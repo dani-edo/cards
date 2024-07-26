3221a237d1e907920c45c3b2e686d00a49436e9b
dani_edo <dani.edogawa.17@gmail.com>
2024-07-27 00:24:42 +0700

    add comments

--- lib/cards.ex
+++ lib/cards.ex
@@ -17,6 +17,7 @@ defmodule Cards do
     # :world
   end
 
+  # create deck and show all deck value
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
@@ -26,23 +27,28 @@ defmodule Cards do
     end
   end
 
-  def suffle(deck) do
+  # shuffle deck value
+  def shuffle(deck) do
     Enum.shuffle(deck)
   end
 
+  # check if deck contains card
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
 
+  # split deck into hand and remaining deck
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
 
+  # save deck to file
   def save(deck, filename) do
     binary = :erlang.term_to_binary(deck)
     File.write(filename, binary)
   end
 
+  # load deck from file
   def load(filename) do
     case File.read(filename) do
       {:ok, binary} -> :erlang.binary_to_term binary
