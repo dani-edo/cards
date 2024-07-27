b9adbf30a950e8e4c868d25f33b34d5d5ab0824f
dani_edo <dani.edogawa.17@gmail.com>
2024-07-27 16:48:49 +0700

    module documentation

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,23 +1,8 @@
 defmodule Cards do
   @moduledoc """
-  Documentation for `Cards`.
+  Provides methods for creaeting and handling a deck of cards
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cards.hello()
-      :world
-
-  """
-  def hello do
-    "hello world"
-    # :world
-  end
-
-  # create deck and show all deck value
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
@@ -27,31 +12,26 @@ defmodule Cards do
     end
   end
 
-  # shuffle deck value
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
 
-  # check if deck contains card
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
 
-  # split deck into hand and remaining deck
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
 
-  # save deck to file
   def save(deck, filename) do
     binary = :erlang.term_to_binary(deck)
     File.write(filename, binary)
   end
 
-  # load deck from file
   def load(filename) do
     case File.read(filename) do
-      {:ok, binary} -> :erlang.binary_to_term binary
+      {:ok, binary} -> :erlang.binary_to_term(binary)
       {:error, _reason} -> "File does not exist"
     end
   end
@@ -63,8 +43,8 @@ defmodule Cards do
     # hand = Cards.deal(deck, hand_size)
 
     # pipe: use chain function with use latest value returned as first argument
-    Cards.create_deck
-    |> Cards.shuffle
+    Cards.create_deck()
+    |> Cards.shuffle()
     |> Cards.deal(hand_size)
   end
 end
